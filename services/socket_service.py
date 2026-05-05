d4c6694727c635ff75c2c38e15c3146408540e2e
sujitfalia-blip <sujitfalia@gmail.com>
2026-05-05 14:29:36 +0530

    Create socket_service.py

---
+++ services/socket_service.py
@@ -0,0 +1,37 @@
+from services.socket_service import socketio
+from models.user import User
+from extensions import db
+from flask import session
+from datetime import datetime
+
+
+# ================= USER CONNECT =================
+@socketio.on('connect')
+def handle_connect():
+
+    user_id = session.get('user_id')
+
+    if user_id:
+        user = User.query.get(user_id)
+
+        if user:
+            user.is_online = True
+            user.last_seen = datetime.utcnow()
+
+            db.session.commit()
+
+
+# ================= USER DISCONNECT =================
+@socketio.on('disconnect')
+def handle_disconnect():
+
+    user_id = session.get('user_id')
+
+    if user_id:
+        user = User.query.get(user_id)
+
+        if user:
+            user.is_online = False
+            user.last_seen = datetime.utcnow()
+
+            db.session.commit()
