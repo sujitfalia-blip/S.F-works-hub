47417653ac93649edabe7e7bad42fe57519a707c
sujitfalia-blip <sujitfalia@gmail.com>
2026-05-06 19:44:59 +0530

    Update main.py

--- routes/main.py
+++ routes/main.py
@@ -1,6 +1,13 @@
 from flask import Blueprint
+from flask import Blueprint, render_template
+
 main = Blueprint('main', __name__)
 
 @main.route("/")
 def home():
     return "🔥 Server Running"
+
+@main.route("/")
+def home():
+    return render_template("login.html")
+    
