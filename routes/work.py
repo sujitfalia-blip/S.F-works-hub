01c4a18aae62241cc80a26ee8d4ba4dec107187d
sujitfalia-blip <sujitfalia@gmail.com>
2026-05-08 18:33:00 +0530

    Update work.py

--- routes/work.py
+++ routes/work.py
@@ -1,7 +1,24 @@
-from services.create_work_service import create_work
+from flask import Blueprint, request, session, redirect
+from services.create_work_service import create_work_service
+
+work = Blueprint("work", __name__)
 
 @work.route('/create', methods=['POST'])
-def create():
+def create_work():
 
-    return create_work(request.form)
-  
+    if 'user_id' not in session:
+        return redirect('/login')
+
+    data = {
+        "title": request.form.get("title"),
+        "workers": request.form.get("workers"),
+        "salary": request.form.get("salary"),
+        "date": request.form.get("date"),
+        "time": request.form.get("time"),
+        "phone": request.form.get("phone")
+    }
+
+    create_work_service(data)
+
+    return "success"
+    
