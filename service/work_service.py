9b688aba86b022539df3f1786785ccf6ece08da4
sujitfalia-blip <sujitfalia@gmail.com>
2026-05-08 17:20:41 +0530

    Create work_service.py

---
+++ service/work_service.py
@@ -0,0 +1,28 @@
+from models.work import Work
+from extensions import db
+from app import socketio   # অথবা তোমার socketio যেখানে define আছে
+
+def create_work(data):
+
+    # ================= CREATE WORK OBJECT =================
+    work = Work(
+        title=data.get("title"),
+        workers=data.get("workers"),
+        salary=data.get("salary"),
+        date=data.get("date"),
+        time=data.get("time"),
+        phone=data.get("phone")
+    )
+
+    # ================= SAVE TO DB =================
+    db.session.add(work)
+    db.session.commit()
+
+    # ================= LIVE UPDATE =================
+    socketio.emit("new_work", {
+        "id": work.id,
+        "title": work.title
+    })
+
+    return work
+  
