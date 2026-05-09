58327ca395346eac3cde5b37aa75de3eb5eb913a
sujitfalia-blip <sujitfalia@gmail.com>
2026-05-10 01:06:36 +0530

    Update profile.py

--- models/profile.py
+++ models/profile.py
@@ -1,4 +1,5 @@
 from extensions import db
+from datetime import datetime
 
 
 class Profile(db.Model):
@@ -9,72 +10,29 @@ class Profile(db.Model):
     user_id = db.Column(
         db.Integer,
         db.ForeignKey('user.id'),
-        unique=True
+        unique=True,
+        nullable=False
     )
 
     user = db.relationship(
         'User',
-        backref='profile',
-        uselist=False
-    )
-    
-
-    id = db.Column(
-        db.Integer,
-        primary_key=True
+        backref=db.backref('profile', uselist=False)
     )
 
-    user_id = db.Column(
-        db.Integer,
-        db.ForeignKey('user.id'),
-        unique=True
-    )
+    name = db.Column(db.String(100))
+    address = db.Column(db.String(200))
+    age = db.Column(db.Integer)
+    education = db.Column(db.String(100))
+    area = db.Column(db.String(100))
+    gender = db.Column(db.String(20))
+    religion = db.Column(db.String(50))
+    country = db.Column(db.String(50))
+    work_desc = db.Column(db.Text)
 
-    name = db.Column(
-        db.String(100)
-    )
+    profile_img = db.Column(db.String(200))
+    cover_img = db.Column(db.String(200))
 
-    address = db.Column(
-        db.String(200)
-    )
+    gallery = db.Column(db.Text)
 
-    age = db.Column(
-        db.Integer
-    )
-
-    education = db.Column(
-        db.String(100)
-    )
-
-    area = db.Column(
-        db.String(100)
-    )
-
-    gender = db.Column(
-        db.String(20)
-    )
-
-    religion = db.Column(
-        db.String(50)
-    )
-
-    country = db.Column(
-        db.String(50)
-    )
-
-    work_desc = db.Column(
-        db.Text
-    )
-
-    profile_img = db.Column(
-        db.String(200)
-    )
-
-    cover_img = db.Column(
-        db.String(200)
-    )
-
-    gallery = db.Column(
-        db.Text
-    )
-    
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
