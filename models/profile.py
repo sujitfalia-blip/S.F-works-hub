18cbe1fcdf33e83e0107c30cad57820a3bef5e43
sujitfalia-blip <sujitfalia@gmail.com>
2026-05-09 16:31:16 +0530

    Update profile.py

--- models/profile.py
+++ models/profile.py
@@ -2,21 +2,63 @@ from extensions import db
 
 
 class Profile(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True)
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
 
-    name = db.Column(db.String(100))
-    address = db.Column(db.String(200))
-    age = db.Column(db.Integer)
-    education = db.Column(db.String(100))
-    area = db.Column(db.String(100))
-    gender = db.Column(db.String(20))
-    religion = db.Column(db.String(50))
-    country = db.Column(db.String(50))
-    work_desc = db.Column(db.Text)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id'),
+        unique=True
+    )
 
-    profile_img = db.Column(db.String(200))
-    cover_img = db.Column(db.String(200))
-    gallery = db.Column(db.Text)
+    name = db.Column(
+        db.String(100)
+    )
+
+    address = db.Column(
+        db.String(200)
+    )
+
+    age = db.Column(
+        db.Integer
+    )
+
+    education = db.Column(
+        db.String(100)
+    )
+
+    area = db.Column(
+        db.String(100)
+    )
+
+    gender = db.Column(
+        db.String(20)
+    )
+
+    religion = db.Column(
+        db.String(50)
+    )
+
+    country = db.Column(
+        db.String(50)
+    )
+
+    work_desc = db.Column(
+        db.Text
+    )
+
+    profile_img = db.Column(
+        db.String(200)
+    )
+
+    cover_img = db.Column(
+        db.String(200)
+    )
+
+    gallery = db.Column(
+        db.Text
+    )
     
