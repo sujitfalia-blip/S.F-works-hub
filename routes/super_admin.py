14132b70bb5e4474f7de89fbee443d1b955c2ce0
sujitfalia-blip <sujitfalia@gmail.com>
2026-05-14 17:45:46 +0530

    Update super_admin.py

--- routes/super_admin.py
+++ routes/super_admin.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from sqlalchemy import func
 from models.user import User
 from models.work_model import Work
+from models.work_application_model import WorkApplication
 from models.activity_log import ActivityLog
 from extensions import db
 from extensions import socketio
