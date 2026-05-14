89c2addc74b208582e870d444c3c2479e3b5741b
sujitfalia-blip <sujitfalia@gmail.com>
2026-05-14 21:46:46 +0530

    Update main.py

--- routes/main.py
+++ routes/main.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-    return "HOME WORKING"
+    return redirect(url_for('auth.login'))
