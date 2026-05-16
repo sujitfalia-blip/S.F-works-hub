a636ccdd1eb0712d6fd4d16a8d24b1a11067a307
sujitfalia-blip <sujitfalia@gmail.com>
2026-05-17 01:39:37 +0530

    Update profile.py

--- routes/profile.py
+++ routes/profile.py
@@ -157,10 +157,21 @@ def my_profile():
 # ================= VIEW OTHER PROFILE =================
 
 import json
+from models import Profile, Work
 
 @profile_bp.route('/profile/<int:user_id>')
 def view_profile(user_id):
 
+    profile = Profile.query.filter_by(user_id=user_id).first_or_404()
+
+    work = Work.query.filter_by(user_id=user_id).first()  # গুরুত্বপূর্ণ line
+
+    return render_template(
+        "profile.html",
+        profile=profile,
+        work=work
+    )
+    
     # ================= USER =================
 
     viewed_user = db.session.get(
