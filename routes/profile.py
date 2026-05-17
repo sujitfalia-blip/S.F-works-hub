8988b5a1d7a80a76949a36c90c2c60b9852a7221
sujitfalia-blip <sujitfalia@gmail.com>
2026-05-17 11:40:01 +0530

    Update profile.py

--- routes/profile.py
+++ routes/profile.py
@@ -161,15 +161,15 @@ def my_profile():
 @profile_bp.route('/profile/<int:user_id>')
 def view_profile(user_id):
 
+    user = User.query.get_or_404(user_id)
     profile = Profile.query.filter_by(user_id=user_id).first_or_404()
     work = Work.query.filter_by(user_id=user_id).first()
-    user = User.query.get_or_404(user_id)
 
     return render_template(
         "profile.html",
+        user=user,
         profile=profile,
-        work=work,
-        user=user
+        work=work
     )
     
     # ================= USER =================
